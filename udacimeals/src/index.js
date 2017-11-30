5ec2fec1247a9051f56d657ef25175e9462822a8
Gil Akos <gil@astra.finance>
2017-11-30 09:55:53 -0800

    Feat: Use react-redux bindings

--- udacimeals/src/index.js
+++ udacimeals/src/index.js
@@ -11,7 +11,6 @@ const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-console.log(store.getState())
 
 ReactDOM.render(
   <Provider store={store}>
